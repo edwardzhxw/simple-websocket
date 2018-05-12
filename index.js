2fa66e4f7e121835cd4ff876ec9923ce9669b6e4
unknown <edwardzhang@italki.com>
2018-05-12 15:49:50 +0800

    init

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+
+const app = express();
+
+//initialize a simple http server
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({ port: 8989 })
+
+const users = []
+
+const broadcast = (data, ws) => {
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify(data))
+    }
+  })
+}
+
+wss.on('connection', (ws) => {
+  let index
+  ws.on('message', (message) => {
+    const data = JSON.parse(message);
+    switch (data.type) {
+      case 'ADD_USER': {
+        index = users.length
+        users.push({ name: data.name, id: index + 1 })
+        ws.send(JSON.stringify({
+          type: 'USERS_LIST',
+          users
+        }))
+        broadcast({
+          type: 'USERS_LIST',
+          users
+        }, ws)
+        break
+      }
+      case 'ADD_MESSAGE':
+        broadcast({
+          type: 'ADD_MESSAGE',
+          message: data.message,
+          author: data.author
+        }, ws)
+        break
+      default:
+        broadcast({
+          type: 'CONNECTED111',
+          message: 'CONN'
+        }, ws);
+        break
+    }
+  })
+
+  ws.on('close', () => {
+    users.splice(index, 1)
+    broadcast({
+      type: 'USERS_LIST',
+      users
+    }, ws)
+  })
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/ws.html');
+})
+
+//start our server
+server.listen(process.env.PORT || 8999, () => {
+    console.log(`Server started on port ${server.address().port} :)`);
+});
